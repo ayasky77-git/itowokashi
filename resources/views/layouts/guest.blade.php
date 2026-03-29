baad228fd0525db8e14f293df4c82a4cdd4879f6
ayasky77-git <ayasky77@gmail.com>
2026-03-30 00:34:28 +0900

    画面サイズ修正

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    {{-- ... (meta, title, Fonts, Scripts) ... --}}
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link rel="icon" type="image/png" href="/favicon.png">
-
 </head>
 <body class="antialiased" 
       style="background-color: #FEF8F0; 
